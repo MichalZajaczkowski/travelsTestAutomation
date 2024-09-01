9b70d39e55f28a009bb966c36d16b382aa7a13ea
mzaja <mzajaczkowski90@gmail.com>
2024-09-01 17:47:09 +0200

    fluent approach

--- src/test/java/org/example/travelsTestAutomation/Sekcja14SeleniumPageObjectPattern/pages/SingUpPage.java
+++ src/test/java/org/example/travelsTestAutomation/Sekcja14SeleniumPageObjectPattern/pages/SingUpPage.java
@@ -1,7 +1,5 @@
 package org.example.travelsTestAutomation.Sekcja14SeleniumPageObjectPattern.pages;
 
-import org.example.travelsTestAutomation.Sekcja14SeleniumPageObjectPattern.model.User;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -25,10 +23,12 @@ public class SingUpPage {
     private static final String DOMAIN = "@example.com";
     private static final Random random = new Random();
     protected WebDriverWait wait;
+    private WebDriver driver;
 
     public SingUpPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.driver = driver;
     }
 
     @FindBy(name="firstname")
@@ -60,28 +60,34 @@ public class SingUpPage {
     private WebElement resultSignup;
 
 
-    public void setFirstname(String firstName) {
+    public SingUpPage setFirstname(String firstName) {
         firstNameInput.sendKeys(firstName);
+        return this;
     }
 
-    public void setLastNames(String lastNames) {
+    public SingUpPage setLastNames(String lastNames) {
         lastNameInput.sendKeys(lastNames);
+        return this;
     }
 
-    public void setPhone(String phone) {
+    public SingUpPage setPhone(String phone) {
         phoneInput.sendKeys(phone);
+        return this;
     }
 
-    public void setEmail(String email) {
+    public SingUpPage setEmail(String email) {
         emailInput.sendKeys(email);
+        return this;
     }
 
-    public void setPassword(String password) {
+    public SingUpPage setPassword(String password) {
         passwordInput.sendKeys(password);
+        return this;
     }
 
-    public void setConfirmPassword(String confirmpassword) {
+    public SingUpPage setConfirmPassword(String confirmpassword) {
         confirmPasswordInput.sendKeys(confirmpassword);
+        return this;
     }
 
     public String getPasswordValue() {
@@ -92,8 +98,9 @@ public class SingUpPage {
         return confirmPasswordInput.getAttribute("value");
     }
 
-    public void singUpBtnClick() {
+    public LoggedUserPage singUp() {
         singUpBtnClick.click();
+        return new LoggedUserPage(driver);
     }
 
     public List<String> getErrors() {
@@ -107,94 +114,6 @@ public class SingUpPage {
         WebElement element = wait.until(ExpectedConditions.visibilityOf(resultSignup));
         Assert.assertTrue(element.isDisplayed(), "Element 'resultSignup' nie jest widoczny.");
         // Sprawdź czy element jest widoczny
-    }
-
-    public void filSingUpForm() {
-        setFirstname(firstName);
-        setLastNames(lastName);
-        setPhone(generateRandomPhoneNumber());
-        setEmail(generateRandomEmail());
-        setPassword(password);
-        setConfirmPassword(password);
-        singUpBtnClick();
-    }
-    public void filSingUpForm2() {
-        firstNameInput.sendKeys(firstName);
-        lastNameInput.sendKeys(lastName);
-        phoneInput.sendKeys(generateRandomPhoneNumber());
-        emailInput.sendKeys(generateRandomEmail());
-        passwordInput.sendKeys(password);
-        confirmPasswordInput.sendKeys(password);
-        singUpBtnClick.click();
-    }
-
-    public void filSingUpForm3(String firstName, String lastName, String phone, String email, String password) {
-        firstNameInput.sendKeys(firstName);
-        lastNameInput.sendKeys(lastName);
-        phoneInput.sendKeys(phone);
-        emailInput.sendKeys(email);
-        passwordInput.sendKeys(password);
-        confirmPasswordInput.sendKeys(password);
-        singUpBtnClick.click();
-    }
-
-    public void filSingUpForm4(User user) {
-        firstNameInput.sendKeys(user.getFirstName());
-        lastNameInput.sendKeys(user.getLastName());
-        phoneInput.sendKeys(user.getPhone());
-        emailInput.sendKeys(user.getEmail());
-        passwordInput.sendKeys(user.getPassword());
-        confirmPasswordInput.sendKeys(user.getPassword());
-        singUpBtnClick.click();
-    }
-
-    String firstName = generateRandomFirstName();
-    String lastName = generateRandomLastName();
-    String password = generateRandomPassword();
-
-    // Metoda do generowania losowego imienia
-    public static String generateRandomFirstName() {
-        return FIRST_NAMES[random.nextInt(FIRST_NAMES.length)];
-    }
-
-    // Metoda do generowania losowego nazwiska
-    public static String generateRandomLastName() {
-        return LAST_NAMES[random.nextInt(LAST_NAMES.length)];
-    }
-
-    // Metoda do generowania losowego numeru telefonu
-    public static String generateRandomPhoneNumber() {
-        StringBuilder phoneNumber = new StringBuilder();
-        phoneNumber.append("+48 "); // Prefiks kraju (Polska)
-        for (int i = 0; i < 9; i++) {
-            phoneNumber.append(random.nextInt(10)); // Dodaj cyfry do numeru telefonu
-            if (i == 2 || i == 5) {
-                phoneNumber.append(" "); // Dodaj spacje po trzeciej i szóstej cyfrze
-            }
-        }
-        return phoneNumber.toString();
-    }
-
-    // Metoda do generowania losowego poprawnego adresu e-mail
-    public static String generateRandomEmail() {
-        String firstName = generateRandomFirstName().toLowerCase();
-        String lastName = generateRandomLastName().toLowerCase();
-        int number = random.nextInt(1000); // Dodaj losowy numer na końcu, aby uniknąć powtórzeń
-        return firstName + "." + lastName + number + DOMAIN;
-    }
-
-    // Metoda do generowania losowego hasła
-    public static String generateRandomPassword() {
-        int passwordLength = 8 + random.nextInt(9); // Losowa długość od 8 do 16 znaków
-        StringBuilder password = new StringBuilder();
-
-        // Dodaj losowo wybrane znaki do hasła
-        for (int i = 0; i < passwordLength; i++) {
-            char randomChar = ALL_CHARACTERS.charAt(random.nextInt(ALL_CHARACTERS.length()));
-            password.append(randomChar);
-        }
-
-        return password.toString();
     }
 
 }
